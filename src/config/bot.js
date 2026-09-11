a1b67aa81fbea9b4daa9751962eb2e2035765e2e
slynxafter75-ship-it <slynxafter75@gmail.com>
2026-09-11 15:04:59 +0100

    Update bot activity name to 'Welcome To Dar Lkbira'

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "", // required by Discord API, not shown in the client
+        name: "Welcome To Dar Lkbira", // required by Discord API, not shown in the client
         state: "",     // this is what people actually see
         type: 4,               // Custom
       },
